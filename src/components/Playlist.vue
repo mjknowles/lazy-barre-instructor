1331d099985919b80ab3b69db2e9b8729771244c
mjknowles <mjknowles23@gmail.com>
2017-05-23 10:29:34 -0500

    First version complete - returning songs to screen

--- src/components/Playlist.vue
+++ src/components/Playlist.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="playlist">
     <h1>Hello, {{ msg }}!</h1>
+
+    <ul id="example-1">
+        <li v-for="track in tracks">
+        {{ track.song }} - {{ track.artist }}
+        </li>
+    </ul>
   </div>
 </template>
 
@@ -9,25 +15,50 @@ export default {
   name: 'playlist',
   data () {
     return {
-      msg: 'Welcome to your playlist'
+      msg: 'Welcome to your playlist',
+      tracks: []
     }
   },
   methods: {
     getSongs () {
-      console.log(this.$route.query.hashFragment)
+      var vm = this
+      var params = this.getUrlParameters(location)
+      var accessToken = params['access_token']
       this.$http.get('https://api.spotify.com/v1/recommendations',
         {
           headers: {
-            'Authorization': 'Bearer ' + this.$route.query.hashFragment
+            'Authorization': 'Bearer ' + accessToken
           },
-          data: {
-            'seed_genres': ['dance'],
+          params: {
+            'seed_genres': 'dance',
             'min_tempo': 120,
             'max_tempo': 125
           }
         }).then((response) => {
-          console.log(response.body)
+          var tracks = response.body.tracks
+          vm.tracks = tracks.map(function (t) {
+            return {
+              song: t.name,
+              artist: t.artists[0].name
+            }
+          })
         })
+    },
+
+    getUrlParameters (location) {
+      if (typeof location === 'undefined') {
+        location = window.location
+      }
+      var hashParams = new function Params () {}()
+      if (location.hash.length === 0) {
+        return hashParams
+      };
+      var hashArray = location.hash.substring(1).split('&')
+      for (var i in hashArray) {
+        var keyValPair = hashArray[i].split('=')
+        hashParams[keyValPair[0]] = keyValPair[1]
+      }
+      return hashParams
     }
   },
   created () {
